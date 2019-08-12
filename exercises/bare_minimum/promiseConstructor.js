cbf6f9c13377e1b1016720cc3cae61ef5d338af0
Han Kim <bagsangcheol@HanKyulKim-MacBook-Air.local>
2019-08-12 08:43:44 -0700

    completed most of bare minimum, need to work on basicChaining

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,51 @@
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
+var cbReview = require('./callbackReview.js')
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  let promise = new Promise(function(resolve, reject) {
+    cbReview.pluckFirstLineFromFile(filePath, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    })
+  })
+
+  promise
+  .then((res) => {return res})
+  .catch((err) => {return err});
+  // console.log("HELLO: ", cbReview.pluckFirstLineFromFile(filePath,callback))
+  // pluckFirstLineFromFileAsync(filePath)
+  // .then(console.log('hiiiii'))
+  // .catch(console.log('error'));
+
+  return promise;
 };
+
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  let promise = new Promise(function(resolve, reject) {
+    cbReview.getStatusCode(url, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    })
+  });
+
+  promise
+  .then((res) => {return res})
+  .catch((err) => {return err});
+
+  return promise;
 };
 
 // Export these functions so we can test them and reuse them in later exercises
